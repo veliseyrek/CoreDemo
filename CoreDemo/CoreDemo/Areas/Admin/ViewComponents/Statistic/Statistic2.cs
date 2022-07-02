80c14b8f27a5a5c9588b17a28e98f4b46ea409ee
veliseyrek <veli47seyrek47@gmail.com>
2022-07-02 19:22:23 +0300

    02.07.2022

---
+++ CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Blogs.OrderByDescending(x => x.BlogID).Select(x => x.BlogTitle).Take(1).FirstOrDefault();
+            return View();
+        }
+    }
+}
