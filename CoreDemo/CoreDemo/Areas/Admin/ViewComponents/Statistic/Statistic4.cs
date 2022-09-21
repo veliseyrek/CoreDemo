a9233082442f52051bc6b85b610b7f4986c4cfd5
veliseyrek <veli47seyrek47@gmail.com>
2022-09-22 00:16:36 +0300

    asd

--- CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
+++ CoreDemo/CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -9,7 +9,7 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
         Context c = new Context();
         public IViewComponentResult Invoke()
         {
-            ViewBag.v1 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.Name);
+            ViewBag.v1 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.Name).FirstOrDefault();
             ViewBag.v2 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.ImageURL).FirstOrDefault();
             ViewBag.v3 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.ShortDescription).FirstOrDefault();
             return View();
